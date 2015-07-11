59403f6eff4061ed4d747f8fa087e7745da44ae1
Christopher Mamian <christophermamian@gmail.com>
2015-07-10 20:51:04 -0700

    AdminRouter routes

--- server.js
+++ server.js
@@ -6,5 +6,21 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+var adminRouter = express.Router();
+
+adminRouter.get('/', function(req, res) {
+	res.send('I am the dashboard');
+});
+
+adminRouter.get('/users', function(req, res) {
+	res.send('I show all the users');
+});
+
+adminRouter.get('/posts', function(req, res) {
+	res.send('I show all the posts');
+});
+
+app.use('/admin', adminRouter);
+
 app.listen(1337);
 console.log('Big Brother is listening!');
